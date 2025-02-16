27f56bdb3ee12c39c215f3988cc5173887b8ef98
zhilshi <zhilshi@users.noreply.github.com>
2025-02-16 11:59:15 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+importimport {{{{ defineConfigdefineConfig }}}} from from from from 'vite''vite''vite''vite';
+importimport react react react from from from '@vitejs/plugin-react''@vitejs/plugin-react''@vitejs/plugin-react''@vitejs/plugin-react';
+importimport {{{{ resolveresolve }}}} from from from from 'path''path''path''path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+// https://vitejs.dev/config/// https://vitejs.dev/config/
+exportexport defaultdefault defaultdefault defaultdefault defineConfigdefineConfig(((({{{{
+  pluginsplugins: [[[[reactreact(((())))]]]],
+  optimizeDepsoptimizeDeps: {{{{
+    excludeexclude: [[[['lucide-react''lucide-react''lucide-react''lucide-react']]]],
+  }}}},
+  buildbuild: {{{{
+    rollupOptions: : {
+      input: : {
+        main: : resolveresolve((((__dirname__dirname, , 'index.html''index.html''index.html''index.html'),,
+      },,
+    },,
+  },,
+  resolve: : {
+    alias: : {
+      '@': : resolveresolve(((__dirname__dirname, , 'srcsrc'),,')),,
+    },,
+  },,
+});;
